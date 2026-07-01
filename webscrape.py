996058874a28cd961a2620164f1246d1aca62625
NERICKSON\Nathan Erickson <erickson.jc.ads@gmail.com>
2026-06-30 23:12:42 -0500

    Change to 2026

--- webscrape.py
+++ webscrape.py
@@ -17,11 +17,11 @@ load_dotenv()
 try:
     LOGIN_EMAIL = os.environ.get("EVENTENY_EMAIL")
     LOGIN_PASSWORD = os.environ.get("EVENTENY_PASSWORD")
-    EVENT_ID = os.environ.get("EVENTENY_EVENT_ID_25")
+    EVENT_ID = os.environ.get("EVENTENY_EVENT_ID_26")
 except Exception:
     LOGIN_EMAIL = st.secrets.get("EVENTENY_EMAIL")
     LOGIN_PASSWORD = st.secrets.get("EVENTENY_PASSWORD")
-    EVENT_ID = st.secrets.get("EVENTENY_EVENT_ID_25")
+    EVENT_ID = st.secrets.get("EVENTENY_EVENT_ID_26")
 
 if not all([LOGIN_EMAIL, LOGIN_PASSWORD, EVENT_ID]):
     raise ValueError("Missing environment variables. Please check your .env file setup.")
