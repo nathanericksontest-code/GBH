e2eb70501384d339ec0aee9e52d0fd37e0f32e18
NERICKSON\Nathan Erickson <erickson.jc.ads@gmail.com>
2026-06-30 20:33:00 -0500

    Fix cash

--- app.py
+++ app.py
@@ -414,15 +414,12 @@ else:
         df_raw["Broad Category Group"] = df_raw["Ticket name"].apply(categorized_label)
         
         # Change price string
-        df_raw["Price_num"] = df_raw["Price"].copy()
-        df_raw["Price_num"] = (df_raw["Price_num"].astype(str).str.replace("$", "", regex=False).str.replace(",", "", regex=False).str.strip())
-        df_raw["Price_num"] = pd.to_numeric(df_raw["Price_num"])
+        df_raw["$ Price"] = df_raw["Price"].copy()
+        df_raw["$ Price"] = (df_raw["$ Price"].astype(str).str.replace("$", "", regex=False).str.replace(",", "", regex=False).str.strip())
+        df_raw["$ Price"] = pd.to_numeric(df_raw["$ Price"])
 
         # 3. Apply your cash payment mapping condition
-        df_raw["Cash"] = np.where(df_raw["Payment source"] == "Cash payment", df_raw["Price_num"], 0.0)
-
-        # 4. Now this line will calculate perfectly without throwing a TypeError!
-        st.markdown(f"Total Cash: ${df_raw['Cash'].sum():,.2f}")    
+        df_raw["Cash"] = np.where(df_raw["Payment source"] == "Cash payment", df_raw["$ Price"], 0.0)
 
         # Global variables required across analytical screens
         start_filter, end_filter = datetime.datetime.now(), datetime.datetime.now()
@@ -526,7 +523,7 @@ else:
                 else:
                     filtered_df = filtered_df.sort_values(by=sort_choice, ascending=is_ascending)
                     
-                display_cols = ["ID", "Status", "Attendee first name", "Attendee last name", "Ticket name", "Broad Category Group", "Check-in time", "Check-in by","Refunded by","Refund date","Payment source","Payout amount","Price","Cash","Price_num"]
+                display_cols = ["ID", "Status", "Attendee first name", "Attendee last name", "Ticket name", "Broad Category Group", "Check-in time", "Check-in by","Refunded by","Refund date","Cash"]
                 available_display_cols = [c for c in display_cols if c in filtered_df.columns]
                 st.dataframe(filtered_df[available_display_cols], width='stretch', hide_index=True)
                 
