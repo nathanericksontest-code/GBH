3ffcddc363d7207246f1367dbbe9f487fd2cdf3a
NERICKSON\Nathan Erickson <erickson.jc.ads@gmail.com>
2026-07-01 08:36:43 -0500

    Update vendor parking

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ def categorized_label(name):
         return "Orange Lot"
     elif "red lot" in name_lower:
         return "Red Lot"
-    elif "vendor" in name_lower and "parking" in name_lower:
+    elif "vendor" in name_lower and "sticker" in name_lower:
         return "Vendor Parking"
     elif "multicolor" in name_lower:
         return "Multiguest Parking"
